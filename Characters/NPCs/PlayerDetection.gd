f54ed01215e75db7f69fd5d44ca2d1a4734b53f0
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2020-01-07 00:49:14 -0300

    Player Detection 2: Line of Sight

--- Characters/NPCs/PlayerDetection.gd
+++ Characters/NPCs/PlayerDetection.gd
@@ -1,6 +1,8 @@
 extends "res://Characters/TemplateCharacter.gd"
 
 const FOV_TOLERANCE = 20
+const MAX_DETECTION_RANGE = 320 #640 in course, length of texture
+
 const RED = Color(1, 0.25, 0.25)
 const WHITE = Color(1, 1, 1)
 
@@ -10,7 +12,7 @@ func _ready():
 	Player = get_node("/root").find_node("Player", true, false)
 
 func _process(delta):
-	if Player_in_FOV():
+	if Player_in_FOV() and Player_in_LOS():
 		$Torch.color = RED
 	else:
 		$Torch.color = WHITE
@@ -21,6 +23,22 @@ func Player_in_FOV():
 	var direction_to_Player = (Player.position - global_position).normalized()
 	
 	if abs(direction_to_Player.angle_to(npc_facing_direction)) < deg2rad(FOV_TOLERANCE):
+		return true
+	else:
+		return false
+
+func Player_in_LOS():
+	
+	var space = get_world_2d().direct_space_state
+	var LOS_obstacle = space.intersect_ray(global_position, Player.position, [self], collision_mask)
+	
+	if not LOS_obstacle:
+		return false
+	
+	var distance_to_player = Player.position.distance_to(global_position) #Player.global_position.dis... in course
+	var Player_in_Range = distance_to_player < MAX_DETECTION_RANGE
+	
+	if LOS_obstacle.collider == Player and Player_in_Range:
 		return true
 	else:
 		return false
