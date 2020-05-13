bd03094ebf6f35a7afd05d2e2a7374277236e3b0
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2020-05-13 02:12:46 -0300

    Moving Guards 2: get_simple_path()

--- Characters/NPCs/Guard.gd
+++ Characters/NPCs/Guard.gd
@@ -1,13 +1,47 @@
 extends "res://Characters/NPCs/PlayerDetection.gd"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var navigation = get_tree().get_root().find_node("Navigation2D", true, false)
+onready var destinations = navigation.get_node("Destinations")
 
-# Called when the node enters the scene tree for the first time.
+var motion
+var possible_destinations
+var path
+
+export var minimun_arrival_distance = 5
+export var walk_speed = 0.5
+
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	possible_destinations = destinations.get_children()
+	make_path()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	navigate()
+
+func navigate():
+	var distance_to_destination = position.distance_to(path[0])
+	if distance_to_destination > minimun_arrival_distance:
+		move()
+	else:
+		update_path()
+
+func move():
+	look_at(path[0])
+	motion = (path[0] - position).normalized() * (MAX_SPEED * walk_speed)
+	if is_on_wall():
+		make_path()
+	move_and_slide(motion)
+	
+func update_path():
+	if path.size() == 1:
+		if $Timer.is_stopped():
+			$Timer.start()
+	else:
+		path.remove(0)
+
+func make_path():
+	var new_destination = possible_destinations[randi() % possible_destinations.size() -1]
+	path = navigation.get_simple_path(position, new_destination.position, false)
+
+func _on_Timer_timeout():
+	make_path()
