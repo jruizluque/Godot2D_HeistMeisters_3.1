21a165292373d59333b7379ad4dd9f7cc880b13d
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2020-06-28 01:59:31 -0300

    Simple Doors

---
+++ Doors/Door.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var can_click = false
+
+func _on_Door_body_entered(body):
+	if body.collision_layer == 1:
+		can_click = true
+	else:
+		open()
+
+
+func _on_Door_body_exited(body):
+	if body.collision_layer == 1:
+		can_click = false
+
+
+func _on_Door_input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		open()
+
+func open():
+	$AnimationPlayer.play("Open")
