a6a10bf8ca6bf73bf5ecc5cfc7a351867ffeb348
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2020-06-29 01:17:44 -0300

    Making Computers

---
+++ Computer/Computer.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var can_click = false
+
+
+
+func _on_Area2D_body_entered(body):
+	can_click = true
+
+
+func _on_Area2D_body_exited(body):
+	can_click = false
+	$CanvasLayer/ComputerPopup.hide()
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		$CanvasLayer/ComputerPopup.popup_centered()
